4dac791153b842bfc44d5000c814ee7f6a36892e
kamatt <kamatt@ttu.ee>
2018-09-25 00:54:07 +0300

    hw2

--- src/main/java/servlet/Util.java
+++ src/main/java/servlet/Util.java
@@ -6,9 +6,9 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.stream.Collectors;
 
-private class Util {
+public class Util {
 
-    private static String asString(InputStream is) throws IOException {
+    public static String asString(InputStream is) throws IOException {
         try (BufferedReader buffer = new BufferedReader(new InputStreamReader(is))) {
             return buffer.lines().collect(Collectors.joining("\n"));
         }
